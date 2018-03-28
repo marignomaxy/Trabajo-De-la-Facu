fbaf286adfcea7fe7114f04992fd4240a275b936
marignomaxy <37854678+marignomaxy@users.noreply.github.com>
2018-03-27 23:28:22 -0300

    Add files via upload

---
+++ Registrarse.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+
+	<title>Registrarse</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="contenedor">
+
+		<header>
+			<!--Parte para Registrarse/Iniciar Sesion -->
+			<div class="superior">
+				<a href="./" class="enlinea">Iniciar Sesion</a> |
+				<a href="./Registrarse.php" class="enlinea" >Registrarse</a>
+			</div>	
+			<div id="Logotipo" class="AcomodarLogo">
+				<!--Logo-->
+				<div class="logo">
+					<img class="imagen" src="logotipo.png" alt="logo">
+				</div>
+			</div>
+
+			<div id="menu">
+				<ul>
+					<li><a href="./index.php">Catalogo De Libros</a></li>
+					<li><a href="">Autores</a></li>
+					<li><a href="">Contacto</a></li>
+				</ul>
+			</div>
+
+		</header>
+
+		<section>
+			<form name="Registro" action="registrarse.php" method="get">
+				
+				
+			</form>
+
+
+		</section>
+	</div>	
+</body>
+</html>
